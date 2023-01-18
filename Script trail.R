219957099edfcf2d80aaebc96a20417f63aa9d1c
Alicelilyarmstrong <alicelilyamrstrong@outlook.com>
2023-01-18 12:17:49 +0000

    some good general Git notes

--- Script trail.R
+++ Script trail.R
@@ -5,3 +5,13 @@ Animals<- c(1,2,3,4,5,6)
 More_animals<- c(7,8,9)
 
 Even_more_animals<- c(10,11,12)
+
+#important G
+
+#"saving" means saving changes#
+
+#"adding" you only have to do once, you are telling Git to watch file
+#and you are telling Git what changes you have made to it#
+
+#"commit" you are telling Git the "saved" changes you've made in
+#R are changes you would like Git to recognize#
