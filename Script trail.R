37d55bc80eac86cd82bf20cc12baed74dddfeb0d
Alicelilyarmstrong <alicelilyamrstrong@outlook.com>
2023-01-18 13:24:29 +0000

    final edits

--- Script trail.R
+++ Script trail.R
@@ -18,3 +18,20 @@ Even_more_animals<- c(10,11,12)
 
 #"push"(represented by the green arrow facing upward) sends 
 #everything to Github#
+
+#aim to commit once very 20mins#
+
+#or whenever you complete a portion of code e.g. a ggplot#
+
+#do not commit big data##
+
+#do not commit anything not editable i.e. PDF, PPT ect, 
+#Git can't reconise these sorts of changes to the docs
+#e.g. you commit a PDF, it will save a whole new ___
+#this will take up lots of space on you comupter
+#Git is perfect for verson control, but not in this instance
+
+#you can tell Github to ingore things that are in Git repository
+#but that you don't want on Git hub using gitignore google this lol#
+
+
